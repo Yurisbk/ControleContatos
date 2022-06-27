1cc3af2821cb647d54eb38ef152f8de39de6c549
CodeZombie <CodeZombie@DESKTOP-TA6ICMU>
2022-06-26 21:34:27 -0300

    Adicionar arquivos de projeto.

---
+++ ControleContatos/Controllers/GerenciarContatos.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleContatos.Controllers
+{
+    public class GerenciarContatos : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CriarContato()
+        {
+            return View();
+        }
+        public IActionResult EditarContato()
+        {
+            return View();
+        }
+        public IActionResult DeletarContatoConfirm()
+        {
+            return View();
+        }
+
+    }
+}
